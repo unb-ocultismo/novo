7698cac02157a471ad6af6a1d22ffc71c0c8a3ee
hanabiworks <77695583+hanabiworks@users.noreply.github.com>
2022-08-28 18:20:52 -0300

    novo

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\Programação\\sites\\hanabiworks-dino\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\Programação\\sites\\hanabiworks-dino\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\Programação\\sites\\hanabiworks-dino\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\Programação\\sites\\hanabiworks-dino\\my-website\\src\\css\\custom.css'),
-];
